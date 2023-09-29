43b357de448931f5eb653c17068506700ae5743a
BryceGunter <suq2mg@virginia.edu>
2023-09-29 14:13:46 -0400

    added Tests for User creation and Home page reachability

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import User
+
 
 # Create your tests here.
+def create_User(name, email):
+    """
+    Create a User with the given `name` and 'email'.
+    """
+    return User.objects.create(user_name=name, user_email=email)
+
+
+class UserModelTests(TestCase):
+    def test_user_creation(self):
+        """
+        Test that User creation works.
+        """
+        name = "dummy"
+        email = "User@virginia.edu"
+        User1 = create_User(name, email)
+        self.assertIsInstance(User1, User)
+
+class HomePageTests(TestCase):
+    def test_home_page_exists(self):
+        """
+        Test that homepage is reachable.
+        """
+        url = reverse("home_page")
+        response = self.client.get(url)
+        self.assertContains(response, "home")
