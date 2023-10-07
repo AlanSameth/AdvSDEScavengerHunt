ed8e99d96e128a0fe5abf0edd68e4fdab6a6ff90
Bryce <suq2mg@virginia.edu>
2023-10-06 23:09:28 -0400

    Edited databases in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'local': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
