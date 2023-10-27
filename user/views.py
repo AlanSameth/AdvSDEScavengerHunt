d3d685244a762e4ff8f56ae0ad033ad677a54c1f
BryceGunter <suq2mg@virginia.edu>
2023-10-27 14:43:34 -0400

    started a view for users creating a game

--- user/views.py
+++ user/views.py
@@ -1,20 +1,25 @@
+from django.db import transaction
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth import authenticate
+
+from user.forms import LocationFormSet
 from user.models import User, location, Game
 from django.urls import reverse
 from django import forms
 from django.http import HttpResponseRedirect
 import googlemaps
 
+
 # Create your views here.
 
 
 class location_form(forms.Form):
-    zipcode = forms.CharField(max_length=400,label="zipcode",required=True)
-    city = forms.CharField(max_length=400,label="city",required=True)
-    country = forms.CharField(max_length=400,label="country",required=True)
-    address = forms.CharField(max_length=400, label="address",required=True)
+    zipcode = forms.CharField(max_length=400, label="zipcode", required=True)
+    city = forms.CharField(max_length=400, label="city", required=True)
+    country = forms.CharField(max_length=400, label="country", required=True)
+    address = forms.CharField(max_length=400, label="address", required=True)
+
 
 def input_location(request):
     if not request.user.is_authenticated:
@@ -28,7 +33,8 @@ def input_location(request):
             loca.country = form.cleaned_data["country"]
             loca.address = form.cleaned_data["address"]
 
-            if location.objects.filter(zipcode=loca.zipcode,city=loca.city,country=loca.country,address=loca.address).first() is not None:
+            if location.objects.filter(zipcode=loca.zipcode, city=loca.city, country=loca.country,
+                                       address=loca.address).first() is not None:
                 # if we can show a message here? like the location is already marked
                 return HttpResponseRedirect(reverse("Map"))
             adress_string = str(loca.address) + ", " + str(loca.zipcode) + ", " + str(
@@ -50,29 +56,52 @@ def input_location(request):
         form = location_form()
     return render(request, "user/new_location.html", {"form": form})
 
+
 def home_page(request):
     if request.user.is_authenticated:
-        user_new=User.objects.filter(user_email=request.user.email).first()
+        user_new = User.objects.filter(user_email=request.user.email).first()
         if user_new is not None:
             user_new.user_name = request.user.username
             user_new.save()
             if user_new.is_admin:
-                return render(request,"user/admin_home.html",{"admin":user_new})
+                return render(request, "user/admin_home.html", {"admin": user_new})
             else:
                 return render(request, "user/home.html")
         else:
-            user_tosave=User(user_name=request.user.username,user_email=request.user.email)
+            user_tosave = User(user_name=request.user.username, user_email=request.user.email)
             user_tosave.save()
     return render(request, "user/home.html")
+
 
 def logout_user(request):
     logout(request)
     return redirect('/')
 
+
 def create_game(request):
-    #figure out template later
+    # figure out template later
     user = request.user
     game = Game(user)
+
+    def get_context_data(self, **kwargs):
+        data = super(create_game, self).get_context_data(**kwargs)
+        if self.request.POST:
+            data['game'] = LocationFormSet(self.request.POST)
+        else:
+            data['game'] = LocationFormSet()
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        game = context['game']
+        with transaction.atomic():
+            self.object = form.save()
+
+            if game.is_valid():
+                game.instance = self.object
+                game.save()
+        return super(create_game, self).form_valid(form)
+
 
 def Map(request):
     if request.user.is_authenticated:
@@ -89,17 +118,11 @@ def Map(request):
                 }
                 locations.append(data)
             print(locations)
-            return render(request, "user/map_admin.html", {"mydata": hidden_location_list,"locations":locations})
+            return render(request, "user/map_admin.html", {"mydata": hidden_location_list, "locations": locations})
         else:
             return render(request, "user/map.html")
     else:
         return HttpResponseRedirect(reverse("Home"))
 
-
-
-
-
-
-
-#learned how to use logout from this URL: https://www.youtube.com/watch?v=yO6PP0vEOMc
+# learned how to use logout from this URL: https://www.youtube.com/watch?v=yO6PP0vEOMc
 # learned more about forms from this URL: https://docs.djangoproject.com/en/4.2/topics/forms/
