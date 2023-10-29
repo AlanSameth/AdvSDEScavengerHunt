38d23343cfa44ebb8862106ba67c43974301e810
tayloraukward <tja8we@virginia.edu>
2023-10-29 11:43:57 -0400

    added is_approved field to game model

--- user/models.py
+++ user/models.py
@@ -4,13 +4,16 @@ from django.db import models
 
 class User(models.Model):
     """ this is the database for user information """
-    user_name=models.CharField(max_length=400)
-    user_email=models.CharField(max_length=400)
-    is_admin=models.BooleanField(default=False)
+    user_name = models.CharField(max_length=400)
+    user_email = models.CharField(max_length=400)
+    is_admin = models.BooleanField(default=False)
+
 
 class Game(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     game_name = models.CharField(max_length=400)
+    is_approved = models.BooleanField(default=False)
+
 
 class location(models.Model):
     """ this is the database for Hidden location information """
@@ -24,4 +27,3 @@ class location(models.Model):
     place_id = models.CharField(max_length=400,blank=True,null=True)
     hint = models.CharField(max_length=400)
     game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
-
